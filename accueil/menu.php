3a74a69c6db14c500c33f4ac681226f2601af891
Arnaud-Loisy <loisy.arnaud@gmail.com>
2013-04-28 15:02:09 +0200

    Ajout de jquery-2.0.0

--- accueil/menu.php
+++ accueil/menu.php
@@ -1,3 +1,23 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Menu</title>
+  <link rel="stylesheet" href="..styles\theme.css">
+  <link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/earthbound" rel="stylesheet" type="text/css"/>
+  <script src="..\script\jquery-2.0.0.js"></script>
+</head>
+
+<body>
+<div id='menu'>
+<ul>
+   <li><a href='#'>Quiz</a></li>
+   <li><a href='#'>Historique</a></li>
+   <li><a href='#'>Matières</a></li>
+   <li><a href='#'>Déconnexion</a></li>
+</ul>
+</div>
+</body>
 <?php
 
 /*
@@ -5,3 +25,4 @@
  * and open the template in the editor.
  */
 ?>
+</html>
