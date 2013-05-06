a9f7d41bf8528502e393f8e50b2943b1082965a0
anthony <>
2013-05-06 16:04:12 +0200

    page d'accueil non connecté et ébauche de page accueil co. V 1.0

---
+++ accueil/accueil.php
@@ -0,0 +1,19 @@
+<html>
+    
+<head>
+     <link rel="stylesheet" href="..\styles\theme.css" />
+    <link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/earthbound" type="text/css"/>
+
+<title>Accueil</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+    <?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+</body>
+<html>
---
+++ accueil/accueil_non_co.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html>
+    
+<head>
+     <link rel="stylesheet" href="..\styles\theme.css" />
+    <link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/earthbound" type="text/css"/>
+
+<title>Accueil</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<form action='traitement.php' method='POST'>
+	Login : <input name="login" type="text"><br>
+	Password : <input name="mdp" type ="password"><br>
+	<input value="Connexion" type="submit"> 
+</body>
+<html>
