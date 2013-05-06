01869c79638fc94401c23f240c503be449898558
Rémi <Rmi@Portable-Rmi>
2013-05-06 17:09:15 +0200

    Création du fichier publication.php

---
+++ session/publication.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<title>Session</title>
+                <link rel="stylesheet" href="..\styles\theme.css" />
+		<link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/earthbound" type="text/css"/>
+        </head>
+<body>
+ <?php   
+// Menu
+include '../accueil/menu.php' 
+    
+// Liste des matières
+
+// liste des Quiz
+
+// Mode de publication
+
+// mdp de la session
+
+// bouton démarrer
+?>
+
+</body>
+
+</html>
