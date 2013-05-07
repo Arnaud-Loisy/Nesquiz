abbb51370f974356a55496c12d6b431a26fe59a1
unknown <loisy.arnaud@gmail.com>
2013-05-07 13:37:55 +0200

    dynamique

--- accueil/menu.php
+++ accueil/menu.php
@@ -1,15 +1,38 @@
 <?php
-if(isset($_SESSION['id']) && ($_SESSION['admin']==1))
+if(isset($_SESSION['id']) && ($_SESSION['statut']==admin))
 {
 	echo"
 <div id='menu'>
 <ul>
-   <li><a href='#'>Jane Smith</a></li>
+   <li><a href='#'>".$_SESSION['prenom']." ".$_SESSION['nom']."</a></li>
    <li><a href='#'>Quiz</a></li>
    <li><a href='#'>Comptes<br>Utilisateurs</a></li>
    <li><a href='#'>Historique</a></li>
    <li><a href='#'>Matières</a></li>
    <li><a href='#'>Statistiques</a></li>   
+   <li><a href='#'>Déconnexion</a></li>
+</ul>
+</div>";
+}elseif(isset($_SESSION['id']) && ($_SESSION['statut']==prof))
+	{
+		echo"
+<div id='menu'>
+<ul>
+   <li><a href='#'>".$_SESSION['prenom']." ".$_SESSION['nom']."</a></li>
+   <li><a href='#'>Quiz</a></li>
+   <li><a href='#'>Statistiques</a></li>   
+   <li><a href='#'>Déconnexion</a></li>
+</ul>
+</div>";
+	}
+elseif (isset($_SESSION['id']) && ($_SESSION['statut']==etu)) 
+{
+	echo"
+<div id='menu'>
+<ul>
+   <li><a href='#'>".$_SESSION['prenom']." ".$_SESSION['nom']."</a></li>
+   <li><a href='#'>Quiz</a></li>
+   <li><a href='#'>Notes</a></li> 
    <li><a href='#'>Déconnexion</a></li>
 </ul>
 </div>";
@@ -26,5 +49,4 @@ else {
 
 
 ?>
-
 
