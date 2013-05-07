59477edc70912988175c8eac5bfc9664dc7b75f6
anthony <>
2013-05-07 13:58:22 +0200

    MODIF des pages de traitement php acceuil co et page d'accueil non connecté . V 1.1.1 Ajout d'un timer pour voir l'erreur de connexion... + du menu a l'acceuil. Rectification des orthographe ca marche mieux si c'est bien redirigé. Micro changement de rien du tout sur le sleep parce que l'erreur ne s'affiche pas... Création d'une variable de session concernant l'erreur de login...

--- accueil/traitement.php
+++ accueil/traitement.php
@@ -34,7 +34,7 @@ if((isset($_POST["login"])) && (isset ($_POST["mdp"]))){
             $_SESSION["nom"]=$arr["nometudiant"];
             $_SESSION["prenom"]=$arr["prenometudiant"];
            
-		//header("Location:./accueil.php");
+		header("Location:./accueil.php");
 	}
 	else{if($_POST["login"]==$tab["idadminprof"] && $_POST["mdp"]==$tab["mdpadminprof"]){
 		$_SESSION["id"] = $_POST["login"];
@@ -46,13 +46,13 @@ if((isset($_POST["login"])) && (isset ($_POST["mdp"]))){
                 else{
                    $_SESSION["statut"]="prof";
                 }
-		//header('Location:./accueil.php');
+		header('Location:./accueil.php');
 	}
         
         else {
 		$_SESSION["erreur_log"]=1;
                  
-		//header('Location:./accueil_non_co.php');
+		header('Location:./accueil_non_co.php');
 	}
         }
 }
