def88d30986c6e865979835b22c086e90ef44a19
Rémi <Rmi@Portable-Rmi>
2013-05-07 15:03:52 +0200

    Premiere version (non fonctionnelle) pour éviter d'avoir à tester en local car ça marche pas ...

--- session/publication.php
+++ session/publication.php
@@ -3,26 +3,51 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8" />
-		<title>Session</title>
+		<title>Publication d'un quiz</title>
                 <link rel="stylesheet" href="..\styles\theme.css" />
 		<link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/earthbound" type="text/css"/>
         </head>
 <body>
- <?php   
-// Menu
-include '../accueil/menu.php' 
-    
-// Liste des matières
+    <div id='page'>
+        <?php 
+        
+        include '../accueil/menu.php';
+        include '../admin/secret.php';
+        
+      /* $dbcon = pg_connect("host=$host user=$login password=$password");
+       //  $request = "SELECT * FROM Matières, AdminProfs, Enseigne WHERE AdminProfs.idAdminProf = Enseigne.idAdminProf AND Matieres.idMatiere = Enseigne.idMatiere AND AdminProfs.idAdminProf = $idAdminProf;";
+        // $result = pg_query($dbcon,$request) or die("Echec de la requête");
+         /*$pg_fetch_array($result)*/
+            
+        ?>
+        <form action="xxx.php" method="POST">
+        <br><br> Mes matières : <br>
+        <?php
+         $idMatiere = 1;
+         $libelleMatiere="matiere courante";
+         
+         
+         
+       //     for($idMatiere;$idMatiere<5;$idMatiere++)
+                echo "<input type='radio' name='matiere' value='".$idMatiere."'> ".$libelleMatiere." <br>";
+        ?>
 
-// liste des Quiz
+        <br> Liste des Quiz : <br>
+        <?
+            $idQuiz = 0;
+            $libelleQuiz = "Quiz de la mort qui tue";
 
-// Mode de publication
-
-// mdp de la session
-
-// bouton démarrer
-?>
-
+            for($idQuiz;$idQuiz<3;$idQuiz++)
+                echo "<input type='radio' name='quiz' value='".$idQuiz."'> ".$libelleQuiz." <br>";
+        ?>
+            <br> Mode de publication : <br>
+            <input type="radio" name="mode" value="1"> Question par Question<br>
+            <input type="radio" name="mode" value="2"> Quiz entier<br>
+            <br> Mot de passe de la session :
+            <input type="text" name="mdpSession"> <br>
+            <input class="bouton" type="submit" value="Démarrer">
+        </form>
+    </div>
 </body>
 
 </html>
