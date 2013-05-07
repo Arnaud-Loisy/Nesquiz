d7b26770b54309f31b21e98ec566baf2df1b5661
unknown <loisy.arnaud@gmail.com>
2013-05-07 15:27:40 +0200

    ajout des liens dans le menu

--- accueil/menu.php
+++ accueil/menu.php
@@ -4,13 +4,13 @@ if(isset($_SESSION['id']) && ($_SESSION['statut']=="admin"))
 	echo"
 <div id='menu'>
 <ul>
-   <li><a href='#'>".$_SESSION['prenom']." ".$_SESSION['nom']."</a></li>
-   <li><a href='#'>Quiz</a></li>
-   <li><a href='#'>Comptes<br>Utilisateurs</a></li>
-   <li><a href='#'>Historique</a></li>
-   <li><a href='#'>Matières</a></li>
-   <li><a href='#'>Statistiques</a></li>   
-   <li><a href='..\accueil\deconnexion.php'>Déconnexion</a></li>
+   <li><a href='../admin/compte.php'>".$_SESSION['prenom']." ".$_SESSION['nom']."</a></li>
+   <li><a href='../quiz/gestionquiz.php'>Quiz</a></li>
+   <li><a href='../admin/gestioncompte.php'>Comptes<br>Utilisateurs</a></li>
+   <li><a href='../admin/historique'>Historique</a></li>
+   <li><a href='../admin/matieres.php'>Matières</a></li>
+   <li><a href='../admin/statistiques.php'>Statistiques</a></li>   
+   <li><a href='../accueil/deconnexion.php'>Déconnexion</a></li>
 </ul>
 </div>";
 }elseif(isset($_SESSION['id']) && ($_SESSION['statut']=="prof"))
@@ -18,10 +18,10 @@ if(isset($_SESSION['id']) && ($_SESSION['statut']=="admin"))
 		echo"
 <div id='menu'>
 <ul>
-   <li><a href='#'>".$_SESSION['prenom']." ".$_SESSION['nom']."</a></li>
-   <li><a href='#'>Quiz</a></li>
-   <li><a href='#'>Statistiques</a></li>   
-   <li><a href='..\accueil\deconnexion.php'>Déconnexion</a></li>
+   <li><a href='../admin/compte.php'>".$_SESSION['prenom']." ".$_SESSION['nom']."</a></li>
+   <li><a href='../quiz/gestionquiz.php'>Quiz</a></li>
+   <li><a href='../admin/statistiques.php'>Statistiques</a></li>   
+   <li><a href='../accueil/deconnexion.php'>Déconnexion</a></li>
 </ul>
 </div>";
 	}
@@ -30,10 +30,10 @@ elseif (isset($_SESSION['id']) && ($_SESSION['statut']=="etu"))
 	echo"
 <div id='menu'>
 <ul>
-   <li><a href='#'>".$_SESSION['prenom']." ".$_SESSION['nom']."</a></li>
-   <li><a href='#'>Quiz</a></li>
-   <li><a href='#'>Notes</a></li> 
-   <li><a href='..\accueil\deconnexion.php'>Déconnexion</a></li>
+   <li><a href='../admin/compte.php'>".$_SESSION['prenom']." ".$_SESSION['nom']."</a></li>
+   <li><a href='../etudiant/listequiz.php'>Quiz</a></li>
+   <li><a href='../etudiant/notes.php'>Notes</a></li> 
+   <li><a href='../accueil/deconnexion.php'>Déconnexion</a></li>
 </ul>
 </div>";
 }
