bef48459f8ce5e5ba1934b0b65014bf9dfcb3743
anthony <>
2013-05-07 13:54:51 +0200

    MODIF des pages de traitement php acceuil co et page d'accueil non connecté . V 1.1.1 Ajout d'un timer pour voir l'erreur de connexion... + du menu a l'acceuil. Rectification des orthographe ca marche mieux si c'est bien redirigé. Micro changement de rien du tout sur le sleep parce que l'erreur ne s'affiche pas... Création d'une variable de session concernant l'erreur de login...

--- accueil/traitement.php
+++ accueil/traitement.php
@@ -22,26 +22,26 @@ if(!$dbcon){
 
 if((isset($_POST["login"])) && (isset ($_POST["mdp"]))){
 	
-	$result_etu= pg_query($dbcon, "SELECT idEtudiant, mdpEtudiant, nomEtudiant, prenomEtudiant FROM Etudiants;");
+	$result_etu= pg_query($dbcon, "SELECT idetudiant, mdpetudiant, nometudiant, prenometudiant FROM etudiants;");
         $arr = pg_fetch_array($result_etu);
-        $result_adm = pg_query($dbcon, "SELECT idAdminProf, mdpAdminProf, Admin, nomAdminProf, prenomAdminProf FROM AdminProfs;");
+        $result_adm = pg_query($dbcon, "SELECT idadminprof, mdpadminprof, admin, nomadminprof, prenomadminprof FROM adminprofs;");
         $tab = pg_fetch_array($result_adm);
         var_dump($arr);
         var_dump($tab);
-	if($_POST["login"]==$arr["idEtudiant"] && $_POST["mdp"]==$arr["mdpEtudiant"]){
+	if($_POST["login"]==$arr["idetudiant"] && $_POST["mdp"]==$arr["mdpetudiant"]){
             $_SESSION["id"] = $_POST["login"];
             $_SESSION["statut"]="etu";
-            $_SESSION["nom"]=$arr["nomEtudiant"];
-            $_SESSION["prenom"]=$arr["prenomEtudiant"];
+            $_SESSION["nom"]=$arr["nometudiant"];
+            $_SESSION["prenom"]=$arr["prenometudiant"];
            
 		//header("Location:./accueil.php");
 	}
-	else{if($_POST["login"]==$tab["idAdminProf"] && $_POST["mdp"]==$tab["mdpAdminProf"]){
+	else{if($_POST["login"]==$tab["idadminprof"] && $_POST["mdp"]==$tab["mdpadminprof"]){
 		$_SESSION["id"] = $_POST["login"];
-                $_SESSION["nom"]=$tab["nomAdminProf"];
-                $_SESSION["prenom"]=$tab["prenomAdminProf"];
+                $_SESSION["nom"]=$tab["nomadminprof"];
+                $_SESSION["prenom"]=$tab["prenomadminprof"];
                 
-                if($tab["Admin"]==t){
+                if($tab["admin"]==t){
                 $_SESSION["statut"] = "admin";}
                 else{
                    $_SESSION["statut"]="prof";
