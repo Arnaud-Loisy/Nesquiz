6324605f97f7dd66e161b6b3938e20f5d2046294
Arnaud-Loisy <loisy.arnaud@gmail.com>
2013-05-07 19:55:30 +0200

    mise en forme

--- accueil/menu.php
+++ accueil/menu.php
@@ -1,11 +1,10 @@
 <?php
-if(isset($_SESSION['id']) && ($_SESSION['statut']=="admin"))
-{
-	echo"
+if (isset($_SESSION['id']) && ($_SESSION['statut'] == "admin")) {
+	echo "
 <div id='menu'>
 <ul>
    <li><a href='../index.php'>Accueil</a></li>
-   <li><a href='../admin/compte.php'>".$_SESSION['prenom']."<br>".$_SESSION['nom']."</a></li>
+   <li><a href='../admin/compte.php'>" . $_SESSION['prenom'] . "<br>" . $_SESSION['nom'] . "</a></li>
    <li><a href='../quiz/gestionquiz.php'>Quiz</a></li>
    <li><a href='../admin/gestioncompte.php'>Comptes<br>Utilisateurs</a></li>
    <li><a href='../admin/historique'>Historique</a></li>
@@ -14,42 +13,36 @@ if(isset($_SESSION['id']) && ($_SESSION['statut']=="admin"))
    <li><a href='../accueil/deconnexion.php'>Déconnexion</a></li>
 </ul>
 </div>";
-}elseif(isset($_SESSION['id']) && ($_SESSION['statut']=="prof"))
-	{
-		echo"
+} elseif (isset($_SESSION['id']) && ($_SESSION['statut'] == "prof")) {
+	echo "
 <div id='menu'>
 <ul>
   <li><a href='../index.php'>Accueil</a></li>
-   <li><a href='../admin/compte.php'>".$_SESSION['prenom']." ".$_SESSION['nom']."</a></li>
+   <li><a href='../admin/compte.php'>" . $_SESSION['prenom'] . " " . $_SESSION['nom'] . "</a></li>
    <li><a href='../quiz/gestionquiz.php'>Quiz</a></li>
    <li><a href='../admin/statistiques.php'>Statistiques</a></li>   
    <li><a href='../accueil/deconnexion.php'>Déconnexion</a></li>
 </ul>
 </div>";
-	}
-elseif (isset($_SESSION['id']) && ($_SESSION['statut']=="etu")) 
-{
-	echo"
+} elseif (isset($_SESSION['id']) && ($_SESSION['statut'] == "etu")) {
+	echo "
 <div id='menu'>
 <ul>
 	<li><a href='../index.php'>Accueil</a></li>
-   <li><a href='../admin/compte.php'>".$_SESSION['prenom']." ".$_SESSION['nom']."</a></li>
-   <li><a href='../etudiant/listequiz.php'>Quiz</a></li>
-   <li><a href='../etudiant/notes.php'>Notes</a></li> 
-   <li><a href='../accueil/deconnexion.php'>Déconnexion</a></li>
+	<li><a href='../admin/compte.php'>" . $_SESSION['prenom'] . " " . $_SESSION['nom'] . "</a></li>
+	<li><a href='../etudiant/listequiz.php'>Quiz</a></li>
+	<li><a href='../etudiant/notes.php'>Notes</a></li> 
+	<li><a href='../accueil/deconnexion.php'>Déconnexion</a></li>
 </ul>
 </div>";
-}
-else {
-	echo"
+} else {
+	echo "
 <div id='menu'>
 <ul>
-   <li><a href='index.php'>Se connecter</a></li>  
-   <li><a href='accueil/inscription.php'>S'inscrire</a></li>
+	<li><a href='accueil/inscription.php'>S'inscrire</a></li>
+	<li><a href='index.php'>Se connecter</a></li>  
 </ul>
 </div>";
 }
-
-
 ?>
 
