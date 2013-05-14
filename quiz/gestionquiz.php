7041ebb3c6e46a41aa1b25113e2c3ccffc79a8ef
jvaquier <jeremy.vaquier@stri-online.com>
2013-05-14 16:12:47 +0200

    Création de la page "gestionquiz.php"

---
+++ quiz/gestionquiz.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+<head>
+<meta charset="utf-8" />
+<title>Gestion des Quiz</title>
+<link rel="stylesheet" href="../styles/theme.css" />
+<link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/earthbound" type="text/css"/>
+
+</head>
+<body>
+<div id='page'>
+<?php
+session_start();
+include '../accueil/menu.php';
+include '../admin/secret.php';
+
+
+	
+?>
+</div>
+</body>
+</html>
