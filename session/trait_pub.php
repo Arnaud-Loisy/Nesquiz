28ee071025ec664a68d6d0c1c3ff81f108b5ca1d
Rémi <boyer.remi81@gmail.com>
2013-05-14 17:34:50 +0200

    modif de supervision pour prendre en compte l'attente des étudiants

--- session/trait_pub.php
+++ session/trait_pub.php
@@ -15,7 +15,7 @@
                 header('Location:../index.php');
             }
             if(!(isset($_GET["mode"])) || !(isset($_GET["idquiz"]))){
-                header('Location: publication.php');
+                header('Location: qsdqdqsd.php');
             }
             
             // Récup des variables
