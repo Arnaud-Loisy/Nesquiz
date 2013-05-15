d2d847959bfd73583df31f00ea98ffaede3c0a21
RAOUX Mathieu <mathieuraoux@gmail.com>
2013-05-15 16:12:56 +0300

    Create compte.php

---
+++ admin/compte.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html>
+    
+<head>
+     <link rel="stylesheet" href="./styles/theme.css" />
+    <link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/earthbound" type="text/css"/>
+
+<title>Gestion Compte</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+En travaux
+
+<html>
