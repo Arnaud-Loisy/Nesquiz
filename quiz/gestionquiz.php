746c7f7bb3e85557c898227ec47623f7f28ba810
jvaquier <jeremy.vaquier@stri-online.com>
2013-05-16 16:28:51 +0200

    Commit Antho

--- quiz/gestionquiz.php
+++ quiz/gestionquiz.php
@@ -8,12 +8,12 @@
 
 
 
-  <link rel="stylesheet" href="..\styles\jquery-ui.css"/>
-  <script src="..\scripts\jquery-2.0.0.js"></script>
-  <script src="..\scripts\jquery-ui.js"></script>
-  <link rel="stylesheet" href="..\styles\theme.css"/>
+  <link rel="stylesheet" href="../styles/jquery-ui.css"/>
+  <script src="../scripts/jquery-2.0.0.js"></script>
+  <script src="../scripts/jquery-ui.js"></script>
+  <link rel="stylesheet" href="../styles/theme.css"/>
   
-  <link rel="stylesheet" href="..\styles\jquery.appendGrid-1.1.0.css"/>
+  <link rel="stylesheet" href="../styles/jquery.appendGrid-1.1.0.css"/>
   <script type="text/javascript" src="../scripts/flexigrid.pack.js"></script>
 
 
@@ -60,7 +60,35 @@ echo "</form>";
     
 
 <script type="text/javascript">
-$('.flexme1').flexigrid();
+$("#flex1").flexigrid({
+	url: 'post2.php',
+	dataType: 'json',
+	colModel : [
+		{display: 'ISO', name : 'iso', width : 40, sortable : true, align: 'center'},
+		{display: 'Name', name : 'name', width : 180, sortable : true, align: 'left'},
+		{display: 'Printable Name', name : 'printable_name', width : 120, sortable : true, align: 'left'},
+		{display: 'ISO3', name : 'iso3', width : 130, sortable : true, align: 'left', hide: true},
+		{display: 'Number Code', name : 'numcode', width : 80, sortable : true, align: 'right'}
+		],
+	buttons : [
+		{name: 'Add', bclass: 'add', onpress : test},
+		{name: 'Delete', bclass: 'delete', onpress : test},
+		{separator: true}
+		],
+	searchitems : [
+		{display: 'ISO', name : 'iso'},
+		{display: 'Name', name : 'name', isdefault: true}
+		],
+	sortname: "iso",
+	sortorder: "asc",
+	usepager: true,
+	title: 'Countries',
+	useRp: true,
+	rp: 15,
+	showTableToggleBtn: true,
+	width: 700,
+	height: 200
+});   
 </script>    
     
     
