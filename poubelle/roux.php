cfb1d47956f96480f1b3f18abca0bbf2fe3876a7
jvaquier <jeremy.vaquier@stri-online.com>
2013-05-17 14:23:57 +0200

    Destruction massive de fichiers

--- poubelle/roux.php
+++ poubelle/roux.php
@@ -67,6 +67,17 @@
 .radioBtns input{margin-top:-5px\9}
 .radioBtns label{background:none\9;padding-bottom:7px\9;padding-left:2px\9;margin-bottom:-5px\9;}
 
+
+
+/*body { width: 500px; text-align: center; font: bold 10px/16px Verdana, sans-serif; color: #555; margin: 20px auto; }*/
+/*h1, p { text-align: left; font-weight: normal; }*/
+/*h1 { font: bold 14px "Trebuchet MS", sans-serif; color: #600; }*/
+table { width: 500px; margin: 0 auto; font-size: 10px; border: 1px solid #ccc; border-width: 1px 0 0 1px; border-collapse: collapse; }
+/*caption { margin: 0 auto; font-size: 12px; margin-bottom: 2em; }*/
+/*td { padding: 10px; border: 1px solid #ccc; border-width: 0 1px 1px 0; }*/
+tr:hover td { background: #CAEFFD; color: #0768B3; cursor: pointer; }
+
+
 </style>
 
 </head>
