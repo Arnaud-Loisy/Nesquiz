ce5faeef5e7abe5f61134c4db85dcf31bc6a6c1b
Arnaud-Loisy <loisy.arnaud@gmail.com>
2013-05-18 14:59:24 +0200

    nettoyage ajout des insertion/delete en bdd

--- session/session.php
+++ session/session.php
@@ -9,25 +9,6 @@
 		<script src="../scripts/jquery-ui.js"></script>
 		<link rel="stylesheet" href="../styles/theme.css" />
 		<link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/earthbound" type="text/css"/>
-
-		<script>
-			$(function() {
-				$("#selectable").selectable({
-					stop : function() {
-						var result = $("#select-result").empty();
-						$(".ui-selected", this).each(function() {
-							var index = $("#select-result").val();
-							//$("#selectable li").index(this);
-
-							result.append("#" + (index));
-							//$(this).load('session.php', {selectable: $('#select-result').html()});
-						});
-					}
-				});
-			});
-		</script>
-		<script></script>
-
 	</head>
 	<body>
 		<div id='page'>
@@ -39,7 +20,11 @@
 
 			include '../accueil/menu.php';
 			include '../admin/secret.php';
-			$session = $_SESSION['idSession'];
+			include '../bdd/connexionBDD.php';
+			include '../bdd/requetes.php';
+			$dateSession = $_SESSION['idSession'];
+			$id=$_SESSION['id'];
+			//DEBUG
 			//var_dump($_POST);
 			//var_dump($_SESSION);
 			if (isset($_POST['precedent'])) {
@@ -48,54 +33,39 @@
 				unset($_POST['precedent']);
 
 				// j'ai cliqué sur « Précedent »
-				$dbcon = pg_connect("host=$host user=$login password=$password");
+				$dbcon = connexionBDD();
 
 				if (!$dbcon) {
 					echo "La connexion à la base de donnée a été perdue<br>";
 				} else {
 
-					$result = pg_query($dbcon, "SELECT questions.idquestion, questions.libellequestion, questions.tempsquestion 
-			FROM   quiz, questions, inclu, sessions
-			WHERE 	  quiz.idquiz = inclu.idquiz 
-			AND	  inclu.idquestion = questions.idquestion 
-			AND	  sessions.idquiz = inclu.idquiz 
-			AND	  sessions.datesession = $session;");
+					$result = pg_query($dbcon, requete_questions_d_une_session($dateSession));
 
+					//positionne sur la bonne question
 					for ($i = 0; $i < $_SESSION['Curseur']; $i++) {
 						$array = pg_fetch_array($result) or die("Echec de la requete3");
 					}
 
-					//var_dump($array);
+					//On met la question courrante dans des variables
 					$question = $array['libellequestion'];
 					$idQuestion = $array['idquestion'];
 					$tempquestion = $array['tempsquestion'];
-					//$_SESSION['QuestionCourrante']=$idQuestion;
-					$array = pg_fetch_array($result) or die("Echec de la requete4");
-					$idQuestionSuivante = $array['idquestion'];
-					//$_SESSION['QuestionSuivante']=$idQuestionSuivante;
 
-					echo '<br><br>Vous avez séléctioné les réponses suivantes : <span id="elect-result"></span>
-			<form method="post" action="session.php">
-				<h1 class="question">' . $question . ' ? :</h1>
-				<ol id="selectable">';
-					$result = pg_query($dbcon, "SELECT questions.idquestion, questions.libellequestion, reponses.idreponse, reponses.libellereponse, questions.tempsquestion 
-			FROM   quiz, questions, inclu, reponses, sessions
-			WHERE 	  quiz.idquiz = inclu.idquiz 
-			AND	  inclu.idquestion = questions.idquestion 
-			AND	  reponses.idquestion = questions.idquestion 
-			AND	  sessions.idquiz = inclu.idquiz 
-			AND	  sessions.datesession = $session
-			AND	  reponses.idquestion =$idQuestion;");
+					echo '<br><br>
+					<form method="post" action="session.php">
+					<h1 class="question">' . $question . ' ? :</h1>
+					<input type="hidden" name="Question" value="' . $idQuestion . '"/>';
+					
+					$result = pg_query($dbcon, requete_reponses_d_une_question_d_une_session($dateSession, $idQuestion));
 					while ($array = pg_fetch_array($result)) {
 
 						$reponse = $array['libellereponse'];
 
-						//idquestion suivante
-						//$array = pg_fetch_array($result);
-						//$idQuestionSuivante = $array['idquestion'];
-						//$_SESSION['QuestionSuivante']=$idQuestionSuivante;
+						$idreponse = $array['idreponse'];
 
-						echo '<li class="ui-widget-content">' . $reponse . '</li>';
+						//echo '<li class="ui-widget-content">' . $reponse . '</li>';
+						echo '<input type="checkbox" name="' . $idreponse . '" value="' . $idreponse . '"/>' . $reponse . '<br>';
+						//echo '<li class="ui-widget-content">' . $reponse . '</li>';
 
 					}
 
@@ -103,59 +73,70 @@
 
 			} elseif (isset($_POST['suivant'])) {
 				$_SESSION['Curseur']++;
-				//$selectable = $_POST['selectable'];
-				//echo "$selectable";
+				$SQLQuestion = $_POST['Question'];
+				unset($_POST['Question']);
 				unset($_POST['suivant']);
 
 				if ($_SESSION['NbdeQuestions'] + 1 > $_SESSION['Curseur']) {
 
 					// j'ai cliqué sur « Suivant »
-					$dbcon = pg_connect("host=$host user=$login password=$password");
+					$dbcon = connexionBDD();
 
 					if (!$dbcon) {
 						echo "La connexion à la base de donnée a été perdue<br>";
 					} else {
 
-						$result = pg_query($dbcon, "SELECT questions.idquestion, questions.libellequestion, questions.tempsquestion 
-			FROM   quiz, questions, inclu, sessions
-			WHERE 	  quiz.idquiz = inclu.idquiz 
-			AND	  inclu.idquestion = questions.idquestion 
-			AND	  sessions.idquiz = inclu.idquiz 
-			AND	  sessions.datesession = $session;");
+						$result = pg_query($dbcon, requete_questions_d_une_session($dateSession));
 
 						for ($i = 0; $i < $_SESSION['Curseur']; $i++) {
 							$array = pg_fetch_array($result) or die("Echec de la requete5");
 						}
 
-						
 						$question = $array['libellequestion'];
 						$idQuestion = $array['idquestion'];
 						$tempquestion = $array['tempsquestion'];
 
-						echo '<br><br>Vous avez séléctioné les réponses suivantes : <span id="elect-result"></span>
-			<form method="post" action="session.php">
-				<h1 class="question">' . $question . ' ? :</h1>
-				<ol id="selectable">';
-						$result = pg_query($dbcon, "SELECT questions.idquestion, questions.libellequestion, reponses.idreponse, reponses.libellereponse, questions.tempsquestion 
-			FROM   quiz, questions, inclu, reponses, sessions
-			WHERE 	  quiz.idquiz = inclu.idquiz 
-			AND	  inclu.idquestion = questions.idquestion 
-			AND	  reponses.idquestion = questions.idquestion 
-			AND	  sessions.idquiz = inclu.idquiz 
-			AND	  sessions.datesession = $session
-			AND	  reponses.idquestion =$idQuestion;");
+						echo '<br><br>
+						<form method="post" action="session.php">
+						<h1 class="question">' . $question . ' ? :</h1>
+						<input type="hidden" name="Question" value="' . $idQuestion . '"/>';
+
+						$result = pg_query($dbcon, requete_reponses_d_une_question_d_une_session($dateSession, $idQuestion));
+
+						$i = 0;
 						while ($array = pg_fetch_array($result)) {
 
 							$reponse = $array['libellereponse'];
+							$idreponse = $array['idreponse'];
 
-							echo '<li class="ui-widget-content">' . $reponse . '</li>';
+							//echo '<li class="ui-widget-content">' . $reponse . '</li>';
+							echo '<input type="checkbox" name="' . $i . '" value="' . $idreponse . '"/>' . $reponse . '<br>';
+							$i++;
+
+						}
+						$result = pg_query($dbcon,efface_les_reponses_d_une_question_d_une_session_d_un_etudiant ($SQLQuestion,$id,$dateSession));
+						foreach ($_POST as $key => $SQLreponse) {
+							
+							$result = pg_query($dbcon, insertion_des_reponses_choisies($SQLQuestion,$SQLreponse,$id,$dateSession));
 
 						}
 
 					}
 
 				} else {
-					header('Location:../session/resultats.php');
+					$dbcon = connexionBDD();
+
+					if (!$dbcon) {
+						echo "La connexion à la base de donnée a été perdue<br>";
+					} else {
+						$result = pg_query($dbcon,efface_les_reponses_d_une_question_d_une_session_d_un_etudiant ($SQLQuestion,$id,$dateSession));
+						foreach ($_POST as $key => $SQLreponse) {
+							
+							$result = pg_query($dbcon, insertion_des_reponses_choisies($SQLQuestion,$SQLreponse,$id,$dateSession));
+
+						}
+						header('Location:../session/resultats.php');
+					}
 				}
 
 			} else {
@@ -163,52 +144,38 @@
 				$_SESSION['Curseur'] = 1;
 
 				// j'arrive sur la page
-				$dbcon = pg_connect("host=$host user=$login password=$password");
+				$dbcon = connexionBDD();
 
 				if (!$dbcon) {
 					echo "La connexion à la base de donnée a été perdue<br>";
 				} else {
 
-					$result = pg_query($dbcon, "SELECT COUNT(*)
-	FROM Questions, Inclu, Quiz, Sessions
-	WHERE	Questions.idQuestion = Inclu.idQuestion
-	AND Quiz.idQuiz = Inclu.idQuiz
-	AND	sessions.idquiz = inclu.idquiz 
-	AND	  sessions.datesession = $session;");
+					$result = pg_query($dbcon, requete_nb_de_questions_d_une_session($dateSession));
 					$array = pg_fetch_array($result) or die("Echec de la requete7");
 					//var_dump($array);
 
 					$_SESSION['NbdeQuestions'] = $array['count'];
 
-					$result = pg_query($dbcon, "SELECT questions.idquestion, questions.libellequestion, questions.tempsquestion 
-			FROM   quiz, questions, inclu, sessions
-			WHERE 	  quiz.idquiz = inclu.idquiz 
-			AND	  inclu.idquestion = questions.idquestion 
-			AND	  sessions.idquiz = inclu.idquiz 
-			AND	  sessions.datesession = $session;");
+					$result = pg_query($dbcon, requete_questions_d_une_session($dateSession));
 
 					$array = pg_fetch_array($result) or die("Echec de la requete1");
 					//var_dump($array);
 					$question = $array['libellequestion'];
 					$idQuestion = $array['idquestion'];
 
-					echo '<br><br>Vous avez séléctioné les réponses suivantes : <span id="elect-result"></span>
-			<form method="post" action="session.php">
-				<h1 class="question">' . $question . ' ? :</h1>
-				<ol id="selectable">';
-					$result = pg_query($dbcon, "SELECT questions.idquestion, questions.libellequestion, reponses.idreponse, reponses.libellereponse, questions.tempsquestion 
-			FROM   quiz, questions, inclu, reponses, sessions
-			WHERE 	  quiz.idquiz = inclu.idquiz 
-			AND	  inclu.idquestion = questions.idquestion 
-			AND	  reponses.idquestion = questions.idquestion 
-			AND	  sessions.idquiz = inclu.idquiz 
-			AND	  sessions.datesession = $session
-			AND	  reponses.idquestion =$idQuestion;");
+					echo '<br><br>
+					<form method="post" action="session.php">
+					<h1 class="question">' . $question . ' ? :</h1>
+					<input type="hidden" name="Question" value="' . $idQuestion . '"/>';
+
+					$result = pg_query($dbcon, requete_reponses_d_une_question_d_une_session($dateSession, $idQuestion));
 					while ($array = pg_fetch_array($result)) {
 
 						$reponse = $array['libellereponse'];
+						$idreponse = $array['idreponse'];
 
-						echo '<li class="ui-widget-content">' . $reponse . '</li>';
+						//echo '<li class="ui-widget-content">' . $reponse . '</li>';
+						echo '<input type="checkbox" name="' . $idreponse . '" value="' . $idreponse . '"/>' . $reponse . '<br>';
 
 					}
 
@@ -217,8 +184,6 @@
 			}
 			?>
 
-			</ol>
-			<input id="select-result" type="hidden" name="reponse" value=""/>
 			<input class="bouton" type="submit" name="precedent" value="Précédent" />
 			<input class="bouton" type="submit" name="suivant" value="Suivant" />
 
