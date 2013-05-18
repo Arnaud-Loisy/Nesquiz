a4e2237a2a536e34afdb0c4ccd7074a75db72869
Arnaud-Loisy <loisy.arnaud@gmail.com>
2013-05-18 20:13:24 +0200

    salle d'attente de lancement session

---
+++ session/session_pause.php
@@ -0,0 +1,40 @@
+<!doctype html>
+
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<title>Session</title>
+		<link rel="stylesheet" href="../styles/theme.css" />
+		<link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/earthbound" type="text/css"/>
+	</head>
+	<body>
+		<div id='page'>
+			<?php
+			session_start();
+			if (!isset($_SESSION["id"]) || (!isset($_SESSION['idSession']))) {
+				header('Location:../index.php');
+			}
+
+			include '../accueil/menu.php';
+			include '../bdd/connexionBDD.php';
+			include '../bdd/requetes.php';
+			$dateSession=$_SESSION['idSession'];
+			$dbcon = connexionBDD();
+			if (!$dbcon) {
+				echo "<br><br>connection échouée à la BDD<br>";
+			} else {
+
+				$result = pg_query($dbcon, requete_mdp_etat_session($dateSession));
+				$array = pg_fetch_array($result);
+				if($array['etatsession']==1){
+					echo "<br><br><br><h1> En attente du lancement de la session</h1>";
+				header("refresh: 5; url=session_pause.php");
+			}else{
+				header('Location:../session/session.php');
+			}
+			
+			}
+			?>
+		</div>
+	</body>
+</html>
