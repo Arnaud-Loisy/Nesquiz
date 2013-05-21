96ed1857dfdd6c6658d76e17b7c9378fd95cad83
jvaquier <jeremy.vaquier@stri-online.com>
2013-05-21 11:52:33 +0200

    Modof requetes.php

--- bdd/requetes.php
+++ bdd/requetes.php
@@ -12,7 +12,7 @@ function requete_tous_quiz_dans_matiere($idMatiere)
 					WHERE Quiz.idQuiz = Inclu.idQuiz
 					AND Questions.idQuestion = Inclu.idQuestion
 					AND Questions.idMatiere = Matieres.idMatiere
-					AND Matieres.idMatiere = " . $idMatiere . ";";
+					AND Matieres.idMatiere = ".$idMatiere.";";
 
 	return $requete;
 }
@@ -22,7 +22,7 @@ function requete_toutes_questions_dans_matiere($idMatiere)
 	$requete = "SELECT libellequestion, tempsquestion, idquestion
 					FROM questions, matieres
 					WHERE questions.idmatiere = matieres.idmatiere
-					AND matieres.idmatiere = " . $idMatiere . ";";
+					AND matieres.idmatiere = ".$idMatiere.";";
 
 	return $requete;
 }
@@ -33,7 +33,7 @@ function requete_toutes_questions_dans_quiz($idQuiz)
                     FROM Questions, Inclu, Quiz
                     WHERE Quiz.idQuiz = Inclu.idQuiz
                     AND Questions.idQuestion = Inclu.idQuestion
-                    AND Quiz.idQuiz = " . $idQuiz . ";";
+                    AND Quiz.idQuiz = ".$idQuiz.";";
 
 	return $requete;
 }
@@ -44,12 +44,12 @@ function requete_toutes_matieres_pour_un_professeur($idAdminProf)
 					FROM Matieres, AdminProfs, Enseigne
 					WHERE AdminProfs.idAdminProf = Enseigne.idAdminProf
 					AND Matieres.idMatiere = Enseigne.idMatiere
-					AND AdminProfs.idAdminProf = " . $idAdminProf . ";";
+					AND AdminProfs.idAdminProf = ".$idAdminProf.";";
 
 	return $requete;
 }
 
-function requete_questions_d_une_session ($session)
+function requete_questions_d_une_session($session)
 {
 	$requete = "SELECT questions.idquestion, questions.libellequestion, questions.tempsquestion 
 				FROM   quiz, questions, inclu, sessions
@@ -60,7 +60,7 @@ function requete_questions_d_une_session ($session)
 	return $requete;
 }
 
-function requete_reponses_d_une_question_d_une_session ($session,$idQuestion)
+function requete_reponses_d_une_question_d_une_session($session, $idQuestion)
 {
 	$requete = "SELECT reponses.idreponse, reponses.libellereponse 
 				FROM   quiz, questions, inclu, reponses, sessions
@@ -73,7 +73,7 @@ function requete_reponses_d_une_question_d_une_session ($session,$idQuestion)
 	return $requete;
 }
 
-function requete_nb_de_questions_d_une_session ($session)
+function requete_nb_de_questions_d_une_session($session)
 {
 	$requete = "SELECT COUNT(*)
 				FROM Questions, Inclu, Quiz, Sessions
@@ -84,17 +84,17 @@ function requete_nb_de_questions_d_une_session ($session)
 	return $requete;
 }
 
-function requete_insertion_des_reponses_choisies ($SQLQuestion,$SQLreponse,$id,$dateSession)
+function requete_insertion_des_reponses_choisies($SQLQuestion, $SQLreponse, $id, $dateSession)
 {
 	$requete = "INSERT INTO repond
-				VALUES (" . $SQLQuestion . ",
-						" . $SQLreponse . ",
-						" . $id . ",
-						" . $dateSession . ");";
+				VALUES (".$SQLQuestion.",
+						".$SQLreponse.",
+						".$id.",
+						".$dateSession.");";
 	return $requete;
 }
 
-function requete_efface_les_reponses_d_une_question_d_une_session_d_un_etudiant ($SQLQuestion,$id,$dateSession)
+function requete_efface_les_reponses_d_une_question_d_une_session_d_un_etudiant($SQLQuestion, $id, $dateSession)
 {
 	$requete = "DELETE FROM repond
  				WHERE idquestion = ".$SQLQuestion."
@@ -103,7 +103,7 @@ function requete_efface_les_reponses_d_une_question_d_une_session_d_un_etudiant 
 	return $requete;
 }
 
-function requete_temps_session ($dateSession)
+function requete_temps_session($dateSession)
 {
 	$requete = "SELECT tempsquiz
 				FROM   quiz, sessions
@@ -120,7 +120,7 @@ function requete_mdp_etat_session($dateSession)
 	return $requete;
 }
 
-function requete_participer_a_une_session($id,$dateSession)
+function requete_participer_a_une_session($id, $dateSession)
 {
 	$requete = "INSERT INTO PARTICIPE values (".$id.",".$dateSession."); ";
 	return $requete;
@@ -136,26 +136,27 @@ function requete_liste_session_ouvertes()
 	return $requete;
 }
 
-
-function requete_libelleQuiz($idQuiz){
-     $request = "SELECT libelleQuiz 
+function requete_libelleQuiz($idQuiz)
+{
+	$request = "SELECT libelleQuiz 
                         FROM quiz
                         WHERE idquiz  = '".$idQuiz."';";
-     return $request;
+	return $request;
 }
 
-function requete_etudiants_participants($dateSession){
-    $request="SELECT Etudiants.nomEtudiant, Etudiants.prenomEtudiant, Etudiants.idEtudiant
+function requete_etudiants_participants($dateSession)
+{
+	$request = "SELECT Etudiants.nomEtudiant, Etudiants.prenomEtudiant, Etudiants.idEtudiant
                                     FROM Etudiants, Sessions, Participe
                                     WHERE Sessions.dateSession = Participe.dateSession
                                     AND Participe.idEtudiant = Etudiants.idEtudiant
                                     AND Sessions.dateSession='".$dateSession."';";
-    return $request;
-
+	return $request;
 }
 
-function requete_nb_questions_repondues_par_un_etudiant($dateSession, $idEtu){
-      $request = "SELECT COUNT (DISTINCT(Questions.idQuestion))
+function requete_nb_questions_repondues_par_un_etudiant($dateSession, $idEtu)
+{
+	$request = "SELECT COUNT (DISTINCT(Questions.idQuestion))
                  FROM Repond, Etudiants, Sessions, Questions, Reponses
                  WHERE Repond.idEtudiant = Etudiants.idEtudiant
                  AND Repond.idQuestion = Questions.idQuestion
@@ -163,53 +164,59 @@ function requete_nb_questions_repondues_par_un_etudiant($dateSession, $idEtu){
                  AND Repond.idReponse = Reponses.idReponse
                  AND Sessions.dateSession='".$dateSession."'
                   AND Etudiants.idEtudiant='".$idEtu."';";
-    return $request;
+	return $request;
 }
 
-function requete_liste_quiz_d_une_matiere($idMatiere){
-     $request="  SELECT DISTINCT Quiz.libelleQuiz, Quiz.idQuiz
+function requete_liste_quiz_d_une_matiere($idMatiere)
+{
+	$request = "  SELECT DISTINCT Quiz.libelleQuiz, Quiz.idQuiz
                         FROM Quiz, Inclu, Questions, Matieres
                         WHERE Quiz.idQuiz = Inclu.idQuiz
                         AND Questions.idQuestion = Inclu.idQuestion
                         AND Questions.idMatiere = Matieres.idMatiere
                         AND Matieres.idMatiere =".$idMatiere.";";
 
-     return $request;
+	return $request;
 }
 
-function requete_changer_etat_session($dateSession,$etatSession){
-     $request="UPDATE Sessions
+function requete_changer_etat_session($dateSession, $etatSession)
+{
+	$request = "UPDATE Sessions
               SET etatSession='".$etatSession."'
               WHERE dateSession='".$dateSession."';";
-     return $request;
+	return $request;
 }
 
-function requete_idQuiz_correspondant_session($dateSession){
-    $request= "SELECT idquiz
+function requete_idQuiz_correspondant_session($dateSession)
+{
+	$request = "SELECT idquiz
                FROM Sessions
                WHERE dateSession='".$dateSession."';";
 
-    return $request;
+	return $request;
 }
 
-function requete_creer_session($dateSession,$modeFonctionnement, $mdpSession, $idquiz, $etatsession){
-    $request = "INSERT INTO sessions VALUES 
+function requete_creer_session($dateSession, $modeFonctionnement, $mdpSession, $idquiz, $etatsession)
+{
+	$request = "INSERT INTO sessions VALUES 
     ('".$dateSession."','".$modeFonctionnement."','".$mdpSession."','".$idquiz."','".$etatsession."');";
 
-    return $request;
+	return $request;
 }
 
-function requete_nb_rep_justes_question($idQuestion){
-    $request="SELECT COUNT(*)
+function requete_nb_rep_justes_question($idQuestion)
+{
+	$request = "SELECT COUNT(*)
                         FROM Reponses, Questions
                         WHERE Reponses.idQuestion = Questions.idQuestion
                         AND Reponses.valide=TRUE
                         AND Questions.idQuestion ='".$idQuestion."';";
-    return $request;
+	return $request;
 }
 
-function requete_nb_rep_fausses_d_un_etudiant_pour_question_d_une_session($idEtu,$idQuestion,$dateSession ){
-    $request="  SELECT COUNT(*)
+function requete_nb_rep_fausses_d_un_etudiant_pour_question_d_une_session($idEtu, $idQuestion, $dateSession)
+{
+	$request = "  SELECT COUNT(*)
                             FROM Repond, Questions, Reponses, Sessions, Etudiants
                             WHERE Repond.idReponse = Reponses.idReponse
                             AND Repond.idQuestion = Questions.idQuestion
@@ -220,11 +227,12 @@ function requete_nb_rep_fausses_d_un_etudiant_pour_question_d_une_session($idEtu
                             AND Questions.idQuestion = '".$idQuestion."'
                             AND Reponses.valide = FALSE;";
 
-    return $request;
+	return $request;
 }
 
-function requete_nb_rep_totales_d_un_etudiant_pour_question_d_une_session($idEtu,$idQuestion,$dateSession ){
-    $request="  SELECT COUNT(*)
+function requete_nb_rep_totales_d_un_etudiant_pour_question_d_une_session($idEtu, $idQuestion, $dateSession)
+{
+	$request = "  SELECT COUNT(*)
                             FROM Repond, Questions, Reponses, Sessions, Etudiants
                             WHERE Repond.idReponse = Reponses.idReponse
                             AND Repond.idQuestion = Questions.idQuestion
@@ -232,80 +240,84 @@ function requete_nb_rep_totales_d_un_etudiant_pour_question_d_une_session($idEtu
                             AND Repond.idEtudiant = Etudiants.idEtudiant	
                             AND Etudiants.idEtudiant = '".$idEtu."'
                             AND Sessions.dateSession = '".$dateSession."'
-                            AND Questions.idQuestion = '".$idQuestion."';"; 
+                            AND Questions.idQuestion = '".$idQuestion."';";
 
-    return $request;
+	return $request;
 }
 
-function requete_nb_questions_d_un_quiz($idQuiz){
-    $request="SELECT COUNT(*)
+function requete_nb_questions_d_un_quiz($idQuiz)
+{
+	$request = "SELECT COUNT(*)
                         FROM Questions, Inclu, Quiz
                         WHERE	Questions.idQuestion = Inclu.idQuestion
                         AND Quiz.idQuiz = Inclu.idQuiz
-                        AND Quiz.idQuiz ='".$idQuiz."';"; 
+                        AND Quiz.idQuiz ='".$idQuiz."';";
 
-    return $request;
+	return $request;
 }
 
-function requete_liste_questions_d_un_quiz($idquiz){
-     $request="SELECT Questions.libelleQuestion, Questions.idQuestion
+function requete_liste_questions_d_un_quiz($idquiz)
+{
+	$request = "SELECT Questions.libelleQuestion, Questions.idQuestion
                         FROM Quiz, Questions, Inclu
                         WHERE Quiz.idQuiz = Inclu.idQuiz
                         AND Questions.idQuestion = Inclu.idQuestion
                         AND Quiz.idQuiz ='".$idquiz."';";
 
-     return $request;
+	return $request;
 }
 
-function requete_liste_sessions_terminees(){
-     $request = "SELECT dateSession, libelleQuiz
+function requete_liste_sessions_terminees()
+{
+	$request = "SELECT dateSession, libelleQuiz
                     FROM Sessions, Quiz
                     WHERE Sessions.idQuiz = Quiz.idQuiz
                     AND Sessions.etatSession = 3
                     ORDER BY Sessions.dateSession DESC;";
 
-     return $request;
+	return $request;
 }
 
-function requete_supprimer_session($dateSession){
-    $request = "DELETE FROM Repond
+function requete_supprimer_session($dateSession)
+{
+	$request = "DELETE FROM Repond
                         WHERE dateSession='".$dateSession."';
                         DELETE FROM Participe
                         WHERE dateSession='".$dateSession."';
                         DELETE FROM Sessions
                         WHERE dateSession='".$dateSession."';";
-    return $request;
+	return $request;
 }
 
-function requete_tous_idadminprof_nomadminprof_prenomadminprof ()
+function requete_tous_idadminprof_nomadminprof_prenomadminprof()
 {
-    $requete = "SELECT idadminprof,nomadminprof,prenomadminprof
+	$requete = "SELECT idadminprof,nomadminprof,prenomadminprof
                 FROM adminprofs
                 ORDER BY nomadminprof";
-    return $requete;
+	return $requete;
 }
 
 function requete_tous_idadminprof($idadminprof)
 {
-    $requete = "SELECT idAdminProf 
+	$requete = "SELECT idAdminProf 
                  FROM AdminProfs 
                  WHERE idAdminProf =".$identifiant;
-    return $requete;
+	return $requete;
 }
 
-function requete_inserer_prof($identifiant,$nom, $prenom,$mdph,$adminb,$langue)
+function requete_inserer_prof($identifiant, $nom, $prenom, $mdph, $adminb, $langue)
 {
-    $requete = "INSERT INTO AdminProfs 
+	$requete = "INSERT INTO AdminProfs 
                 VALUES (".$identifiant.", '".$nom."', '".$prenom."','".$mdph."','".$adminb."','".$langue."');";
-    return $requete;
+	return $requete;
 }
 
 function requete_supprimer_prof($iadminprof)
 {
-    $requete = "DELETE
+	$requete = "DELETE
                 FROM adminprofs
                 WHERE idadminprof ='".$idadminprof."'";
-    return $requete;
+	return $requete;
 }
 
 function requete_toutes_matieres_d_un_etudiant($idEtu)
@@ -321,6 +333,7 @@ function requete_toutes_matieres_d_un_etudiant($idEtu)
 				ORDER BY matieres.libellematiere ASC;";
 	return $requete;
 }
+
 function requete_nombre_de_sessions_d_un_etudiant($idEtu)
 {
 	$requete = "SELECT count (*) 
@@ -329,7 +342,7 @@ function requete_nombre_de_sessions_d_un_etudiant($idEtu)
 	return $requete;
 }
 
-function requete_nombre_de_sessions_d_un_etudiant_matiere_donnee($idEtu,$idMatiere)
+function requete_nombre_de_sessions_d_un_etudiant_matiere_donnee($idEtu, $idMatiere)
 {
 	$requete = "SELECT COUNT(DISTINCT (participe.datesession))
 				FROM   public.participe,   public.inclu,   public.sessions,   public.questions
@@ -349,7 +362,7 @@ function requete_sessions_d_un_etudiant($idEtu)
 	return $requete;
 }
 
-function requete_sessions_d_un_etudiant_par_matiere($idEtu,$idMatiere)
+function requete_sessions_d_un_etudiant_par_matiere($idEtu, $idMatiere)
 {
 	$requete = "SELECT DISTINCT(participe.datesession)
 				FROM  participe, inclu, sessions, questions
