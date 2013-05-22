273d09a68a9a4c030fa8ddf7f2aef23f0b3c0755
anthony <anthony.therond@gmail.com>
2013-05-22 10:31:49 +0200

    Modif profil et trait_profil pour ajout conf mdp et  erreurs soit plus clair pour  l'user

--- accueil/trait_profil.php
+++ accueil/trait_profil.php
@@ -1,90 +1,84 @@
 <?php
+
 include '../admin/secret.php';
 session_start();
-$dbcon=pg_connect("host=$host user=$login password=$password");
+$dbcon = pg_connect("host=$host user=$login password=$password");
 
-if(!$dbcon){
- echo "connection BDD failed<br>";
-}else
-	echo "connection BDD succes <br>";
- 
-if ((isset($_SESSION["statut"]))&& ($_SESSION["statut"]=="etu")){
- $id=$_SESSION["id"];
-        $result_etu= pg_query($dbcon, "SELECT langueEtudiant, mdpetudiant FROM Etudiants WHERE idEtudiant=".$id);
-             $arr = (pg_fetch_array($result_etu));
-                    $langue=$arr["langueEtudiant"];
-                    $mdph=$arr["mdpetudiant"];
-                                                         
-            
-  if((isset($_POST["oldmdp"])) && (isset($_POST["newmdp"])) && (isset($_POST["cnewmdp"]))){
-      $mdpnew=($_POST["newmdp"]);
-      $mdpold=($_POST["oldmdp"]);
-      $mdpcnew=($_POST["cnewmdp"]);
-      $mdphcnew=md5($mdpcnew);
-      $mdphold=md5($mdpold);
-      $mdphnew=md5($mdpnew);
-     // if($mdphcnew!=$mdphnew){ ERREUR A PRENDRE EN COMPTE
-        
-       if($mdph==$mdphold){
-           if($mdphcnew==$mdphnew){
-          pg_query($dbcon, "UPDATE Etudiants SET mdpEtudiant = '".$mdphnew."' WHERE idEtudiant=".$id);   
-          $_SESSION["mdpchok"]=1;
-       }
-       else {
-           $_SESSION["mdpconffail"]=1;
-       }
-       }
-       else {
-           $_SESSION["mdpfail"]=1;
-       }
-      }
-     
-     else {
-           $_SESSION["mdpchfail"]=1;
-       }
-
-  if(isset($_POST["langue"])){
-      $langue=$_POST["langue"];
-  
-     pg_query($dbcon, "UPDATE Etudiants SET langueEtudiant = '".$langue."' WHERE idEtudiant=".$id);
-      $_SESSION["languechok"]=1;
-  }
-  
+if (!$dbcon) {
+    echo "connection BDD failed<br>";
 }
-if (((isset($_SESSION["statut"]))&& ($_SESSION["statut"]=="admin")) || ((isset($_SESSION["statut"]))&& ($_SESSION["statut"]=="prof")) ){
- $id=$_SESSION["id"];
-        $result_adm= pg_query($dbcon, "SELECT langueAdminProf, mdpadminprof FROM AdminProfs WHERE idAdminProf =".$id);
-             $arr = (pg_fetch_array($result_adm));
-                    $langue=$arr["langueAdminProf"];
-                    $mdph=$arr["mdpadminprof"];
-                                                         
-            
-  if((isset($_POST["oldmdp"])) && (isset($_POST["newmdp"])) ){
-      $mdpnew=($_POST["newmdp"]);
-      $mdpold=($_POST["oldmdp"]);
-      $mdphold=md5($mdpold);
-      $mdphnew=md5($mdpnew);
-    
-       if($mdph==$mdphold){
-          pg_query($dbcon, "UPDATE AdminProfs SET mdpAdminProf ='".$mdphnew."' WHERE idAdminProf=".$id); 
-          $_SESSION["mdpchok"]=1;
-       }
-       else {
-           $_SESSION["mdpfail"]=1;
-       }
-  }
-  else {
-           $_SESSION["mdpchfail"]=1;
-       }
-  
-  if(isset($_POST["langue"])){
-      $langue=$_POST["langue"];
-  
-     pg_query($dbcon, "UPDATE AdminProfs SET langueAdminProf = ".$langue." WHERE idAdminProf=".$id);
-       $_SESSION["languechok"]=1;
-  }
-          
-}
-    header('Location:./profil.php');
+else
+    echo "connection BDD succes <br>";
 
+if ((isset($_SESSION["statut"])) && ($_SESSION["statut"] == "etu")) {
+    $id = $_SESSION["id"];
+    $result_etu = pg_query($dbcon, "SELECT langueEtudiant, mdpetudiant FROM Etudiants WHERE idEtudiant=" . $id);
+    $arr = (pg_fetch_array($result_etu));
+    $langue = $arr["langueEtudiant"];
+    $mdph = $arr["mdpetudiant"];
+
+
+    if ((isset($_POST["oldmdp"])) && (isset($_POST["newmdp"])) && (isset($_POST["cnewmdp"]))) {
+        $mdpnew = ($_POST["newmdp"]);
+        $mdpold = ($_POST["oldmdp"]);
+        $mdpcnew = ($_POST["cnewmdp"]);
+        if ( $mdpnew!="" && $mdpold!="" && $mdpcnew!=""){
+        $mdphcnew = md5($mdpcnew);
+        $mdphold = md5($mdpold);
+        $mdphnew = md5($mdpnew);
+        // if($mdphcnew!=$mdphnew){ ERREUR A PRENDRE EN COMPTE
+
+        if ($mdph == $mdphold) {
+            if ($mdphcnew == $mdphnew) {
+                pg_query($dbcon, "UPDATE Etudiants SET mdpEtudiant = '" . $mdphnew . "' WHERE idEtudiant=" . $id);
+                $_SESSION["mdpchok"] = 1;
+            } else {
+                $_SESSION["mdpconffail"] = 1;
+            }
+        } else {
+            $_SESSION["mdpfail"] = 1;
+        }
+    } else {
+        $_SESSION["mdpchfail"] = 1;
+    }
+  }
+    if (isset($_POST["langue"])) {
+        $langue = $_POST["langue"];
+
+        pg_query($dbcon, "UPDATE Etudiants SET langueEtudiant = '" . $langue . "' WHERE idEtudiant=" . $id);
+        $_SESSION["languechok"] = 1;
+    }
+}
+if (((isset($_SESSION["statut"])) && ($_SESSION["statut"] == "admin")) || ((isset($_SESSION["statut"])) && ($_SESSION["statut"] == "prof"))) {
+    $id = $_SESSION["id"];
+    $result_adm = pg_query($dbcon, "SELECT langueAdminProf, mdpadminprof FROM AdminProfs WHERE idAdminProf =" . $id);
+    $arr = (pg_fetch_array($result_adm));
+    $langue = $arr["langueAdminProf"];
+    $mdph = $arr["mdpadminprof"];
+
+
+    if ((isset($_POST["oldmdp"])) && (isset($_POST["newmdp"]))) {
+        $mdpnew = ($_POST["newmdp"]);
+        $mdpold = ($_POST["oldmdp"]);
+        $mdphold = md5($mdpold);
+        $mdphnew = md5($mdpnew);
+
+        if ($mdph == $mdphold) {
+            pg_query($dbcon, "UPDATE AdminProfs SET mdpAdminProf ='" . $mdphnew . "' WHERE idAdminProf=" . $id);
+            $_SESSION["mdpchok"] = 1;
+        } else {
+            $_SESSION["mdpfail"] = 1;
+        }
+    } else {
+        $_SESSION["mdpchfail"] = 1;
+    }
+
+    if (isset($_POST["langue"])) {
+        $langue = $_POST["langue"];
+
+        pg_query($dbcon, "UPDATE AdminProfs SET langueAdminProf = " . $langue . " WHERE idAdminProf=" . $id);
+        $_SESSION["languechok"] = 1;
+    }
+}
+header('Location:./profil.php');
 ?>
