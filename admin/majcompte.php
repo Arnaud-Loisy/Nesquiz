31181fc8d5345604a0a0cb0d60cdb98284c93152
mathieu <mathieuraoux@gmail.comt>
2013-05-22 13:45:13 +0200

    Ajout fonctionnalité

--- admin/majcompte.php
+++ admin/majcompte.php
@@ -1,16 +1,15 @@
 <?php
 
-session_start();
+session_start ();
 include '../bdd/connexionBDD.php';
 include '../bdd/requetes.php';
 
-$dbcon = connexionBDD();
+$dbcon = connexionBDD ();
 
-foreach($_POST['supprimer'] as $idadminprof)
-    if($idadminprof!=0)
-        pg_query($dbcon,requete_supprimer_prof($idadminprof));
-foreach($_POST['admin'] as $idadminprof)
-            pg_query($dbcon,requete_prof_devient_admin($idadminprof));
-header('Location:./compte.php');
-
+foreach ($_POST['supprimer'] as $idadminprof)
+    if ($idadminprof != 0)
+        pg_query ($dbcon, requete_supprimer_prof ($idadminprof));
+foreach ($_POST['admin'] as $idadminprof)
+        pg_query ($dbcon, requete_prof_devient_admin ($idadminprof));
+header ('Location:./compte.php');
 ?>
