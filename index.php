64d84e9f14ae716484081e5a994b0016594aa501
Arnaud-Loisy <loisy.arnaud@gmail.com>
2013-05-23 22:17:12 +0200

    720

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 <title>Accueil</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="width=720">
 </head>
 <body>
     <div id="page">
