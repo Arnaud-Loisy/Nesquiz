ea040457349c90a30b060d88a74b9a2c814a06e3
Arnaud-Loisy <loisy.arnaud@gmail.com>
2013-05-23 22:10:41 +0200

    test2

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 <title>Accueil</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="viewport" content="initial-scale=1.0">
+<meta name="viewport" content="width=device-width">
 </head>
 <body>
     <div id="page">
