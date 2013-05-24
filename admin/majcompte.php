ed19c40c91c70503186e1a1b5d8b34eb0fad7f9c
mathieu <mathieuraoux@gmail.comt>
2013-05-24 11:34:05 +0200

    Modification MAJcompte pour le check box

--- admin/majcompte.php
+++ admin/majcompte.php
@@ -6,10 +6,28 @@ include '../bdd/requetes.php';
 
 $dbcon = connexionBDD ();
 
+var_dump ($_POST['admin']);
+
 foreach ($_POST['supprimer'] as $idadminprof)
     if ($idadminprof != 0)
         pg_query ($dbcon, requete_supprimer_prof ($idadminprof));
+    
 foreach ($_POST['admin'] as $idadminprof)
-        pg_query ($dbcon, requete_prof_devient_admin ($idadminprof));
+    pg_query ($dbcon, requete_prof_devient_admin ($idadminprof));
+
+$resultatIdadmin = pg_query ($dbcon, "SELECT idadminprof FROM adminprofs WHERE admin = 'true' ORDER BY idadminprof;");
+while ($arr = pg_fetch_array ($resultatIdadmin)) {
+    $idadminFromBD = $arr['idadminprof'];
+    echo $idadminFromBD;
+    $valider = 'false';
+    foreach ($_POST['admin'] as $idadminprof)
+        if ($idadminprof == $idadminFromBD)
+            $valider = 'true';
+    if ($valider != 'true')
+        pg_query ($dbcon, requete_admin_devient_prof ($idadminprof));
+}
+
+
+//        pg_query ($dbcon, requete_admin_devient_prof ($idadminprof));
 header ('Location:./compte.php');
 ?>
