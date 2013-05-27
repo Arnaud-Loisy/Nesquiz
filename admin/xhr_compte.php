fe1977e4ef15f9a9aec39ca6865e85b4a1e95d12
mathieu <mathieuraoux@gmail.comt>
2013-05-27 15:23:56 +0200

    Ajout des menus

--- admin/xhr_compte.php
+++ admin/xhr_compte.php
@@ -6,32 +6,34 @@ include '../bdd/requetes.php';
 $dbcon = connexionBDD ();
 
 if ($_POST['utilisateur'] == "prof") {
+    unset ($_SESSION["etudiant"]);
+    $_SESSION['enseignant'] = 1;
     echo"
         <br>
-        <table class='border'>
-            <tr>
-                <td class='identifiant'> Identifiant </td>
-                <td class='nom'> Nom </td> 
-                <td class='prenom'> Prenom </td> 
-                <td class='admin'> Admin </td> 
-                <td class='supprimer'> Supprimer </td>
-            </tr>
-        </table>";
+        <form action='majcompte.php' method='POST'>
+        <div class='scroll'>
+            <table class='liste'>
+                <thead>
+                    <th class='identifiant'> Identifiant </th>
+                    <th class='nom'> Nom </th> 
+                    <th class='prenom'> Prenom </th> 
+                    <th class='admin'> Admin </th> 
+                    <th class='supprimer'> Supprimer </th>
+                </thead>
+                <tbody>";
+    
     // Récupère l'identifiant, le nom et le prénom des admins et des profs
     $resultat = pg_query ($dbcon, requete_tous_idadminprof_nomadminprof_prenomadminprof ());
-    echo"
-<form action='majcompte.php' method='POST'>
-<div class='scroll'>
-<table class = 'border'>";
+    
     while ($arr = pg_fetch_array ($resultat)) {
         $nomadminprof = $arr["nomadminprof"];
         $prenomadminprof = $arr["prenomadminprof"];
         $idadminprof = $arr["idadminprof"];
         echo"
-<tr>
-<td class='identifiant'>" . $idadminprof . "</td>
-<td class='nom'>" . $nomadminprof . "</td>
-<td class='prenom'>" . $prenomadminprof . "</td>";
+            <tr>
+                <td class='identifiant'>" . $idadminprof . "</td>
+                <td class='nom'>" . $nomadminprof . "</td>
+                <td class='prenom'>" . $prenomadminprof . "</td>";
         $resultatAdmin = pg_query ($dbcon, requete_si_admin ($idadminprof));
         $arrAdmin = pg_fetch_array ($resultatAdmin);
         $admin = $arrAdmin['admin'];
@@ -43,20 +45,46 @@ if ($_POST['utilisateur'] == "prof") {
                 <td class='admin'><input type='checkbox' name='admin[]' value='" . $idadminprof . "' ></td>";
 
         echo"
-            <td class='supprimer'><input type='checkbox' name='supprimer[]' value='" . $idadminprof . "'></td>
-</tr>";
+                        <td class='supprimer'><input type='checkbox' name='supprimer[]' value='" . $idadminprof . "'></td>
+                    </tr>";
+        
     }
+    echo"
+                </tbody>
+            </table>
+        </div>
+        <br>
+        <input  class='boutonMAJ' value='Valider' type='submit'>";
 }
 else {
+    unset ($_SESSION["enseignant"]);
+    $_SESSION['etudiant'] = 1;
     echo"
         <br>
-        <table class='border'>
+        <form action='majcompte.php' method='POST'>
+            <div class='scroll'>
+                <table class='liste'>
+                    <thead>
+                        <th class='identifiant'>N° Etudiant</th>
+                        <th class='nom'>Nom</th>
+                        <th class='prenom'>Prenom</th>
+                        <th class='admin'>Promo</th>
+                        <th class='supprimer'>Supprimer</th>
+                    </thead>
+                <tbody>";
+    $resultat = pg_query ($dbcon, requete_tous_les_etudiants());
+    while ($arr = pg_fetch_array ($resultat)) {
+        $numero_etu = $arr['idetudiant'];
+        $nom = $arr['nometudiant'];
+        $prenom = $arr['prenometudiant'];
+        $promo = $arr['promo'];
+        echo"
             <tr>
-                <td class='identifiant'> N° Etudiant </td>
-                <td class='nom'> Nom </td> 
-                <td class='prenom'> Prenom </td> 
-                <td class='admin'> Admin </td> 
-                <td class='supprimer'> Supprimer </td>
-            </tr>
-        </table>";
+                <td class='identifiant'>" .$numero_etu. "</td>
+                <td class='nom'>" .$nom. "</td>
+                <td class='prenom'>" .$prenom. "</td>
+                <td class='admin'>".$promo. "</td>
+                <td class='supprimer'><input type='checkbox' name='supprimer[]' value='" .$numero_etu. "'></td>
+           </tr>";
+    }
 }
