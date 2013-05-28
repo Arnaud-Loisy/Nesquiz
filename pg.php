ae8db43736b6797d680decc81e8dcf3fcc4a7fc2
Arnaud-Loisy <loisy.arnaud@gmail.com>
2013-05-28 19:04:58 +0200

    test bdd

--- pg.php
+++ pg.php
@@ -5,9 +5,9 @@ $dbcon=pg_connect("host=$host user=$login password=$password");
 if(!$dbcon){
  echo "connection failed<br>";
 }else
-	echo "connection not failed<br>";
+	echo "connection BDD OK <br>";
 
-$result = pg_query($dbcon, "SELECT nom FROM test");
+$result = pg_query($dbcon, "SELECT nometudiant FROM etudiants");
 $arr = pg_fetch_array($result);
 var_dump($arr);
 echo "<br>";
