134cfbfc7dfd830f45d9da26c55f3ac4def0dd39
unknown <loisy.arnaud@gmail.com>
2013-05-28 17:53:13 +0200

    Menu Test

--- admin/statistiques.php
+++ admin/statistiques.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8" />
+		<meta name="viewport" content="width=720">
 		<title>Notes</title>
 		<link rel="stylesheet" href="../styles/theme.css" />
 		<script type='text/javascript'>
