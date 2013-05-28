90e6e220c06fc3c46b675b1d411503bf0b0b15b2
anthony <anthony.therond@gmail.com>
2013-05-28 15:03:15 +0200

    Prise en compte des quotes et double quotes dans les requetes.

--- accueil/inscription.php
+++ accueil/inscription.php
@@ -34,6 +34,8 @@
             }
                 if (isset( $_SESSION["erreur_longeur_champ_inscription_prenom"])){
                        $str= "juiu'huhi'uhihih";
+                       $test=strpos($str,"");
+                       echo $test;
             echo htmlentities($str,ENT_NOQUOTES);
             echo"<br>Erreur:La longueur maximale des champs est de 32 caratères. <br>";
             echo "<br>Veuillez réduire le champ 'prenom' qui est  trop long.<br>";
