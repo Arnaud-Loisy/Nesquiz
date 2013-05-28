d2d95a26c3ef64dc9c8e6fddc1d9e07f6540d1b5
anthony <anthony.therond@gmail.com>
2013-05-28 14:31:57 +0200

    Prise en compte des quotes et double quotes dans les requetes.

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     if(isset($_SESSION["erreur_log"])){
         echo"Identifiant et/ou Mot de passe erroné";
            $str=$_POST["login"];
-            echo htmlentities($str,ent_quotes);
+            echo htmlentities($str,ENT_QUOTES);
          unset($_SESSION["erreur_log"]);  
        }    
     if (isset($_SESSION["id"])){
