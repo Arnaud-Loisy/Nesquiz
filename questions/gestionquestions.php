611a978b1197de66e3359c681329b9c717c895fa
jvaquier <jeremy.vaquier@stri-online.com>
2013-05-30 00:06:24 +0200

    Bug warning session_start()

--- questions/gestionquestions.php
+++ questions/gestionquestions.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="fr">
     <head>
-        <meta charset="utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>Gestion des Questions</title>
         <link rel="stylesheet" href="../styles/theme.css" />
         <script type='text/javascript'>
