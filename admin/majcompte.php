d137996e8e13e3c06e8ba0886e51863c3671b1b4
mathieu <mathieuraoux@gmail.comt>
2013-05-30 01:11:46 +0200

    Page fini, ajout suppresion étudiant

--- admin/majcompte.php
+++ admin/majcompte.php
@@ -11,24 +11,31 @@ $dbcon = connexionBDD ();
 if (!$dbcon)
     echo "<br><br>connection échouée à la BDD<br>";
 else {
-    // Suppression de certains enseignants sauf 0
-    foreach ($_POST['supprimer'] as $idadminprof)
-        if ($idadminprof != '0')
-            pg_query ($dbcon, requete_supprimer_prof ($idadminprof));
-    // Certains enseignants deviennent admin    
-    foreach ($_POST['admin'] as $idadminprof)
-        pg_query ($dbcon, requete_prof_devient_admin ($idadminprof));
-
-    $resultatIdadmin = pg_query ($dbcon, "SELECT idadminprof FROM adminprofs WHERE admin = 'true' ORDER BY idadminprof;");
-    while ($arr = pg_fetch_array ($resultatIdadmin)) {
-        $idadminFromBD = $arr['idadminprof'];
-        $valider = 'false';
+    if (isset ($_SESSION['enseignant'])) {
+        // Suppression de certains enseignants sauf 0
+        foreach ($_POST['supprimer'] as $idadminprof)
+            if ($idadminprof != '0')
+                pg_query ($dbcon, requete_supprimer_prof ($idadminprof));
+        // Certains enseignants deviennent admin    
         foreach ($_POST['admin'] as $idadminprof)
-            if ($idadminprof == $idadminFromBD)
-                $valider = 'true';
-        if ($valider != 'true' && $idadminFromBD != '0')
-            pg_query ($dbcon, requete_admin_devient_prof ($idadminFromBD));
+            pg_query ($dbcon, requete_prof_devient_admin ($idadminprof));
+
+        $resultatIdadmin = pg_query ($dbcon, "SELECT idadminprof FROM adminprofs WHERE admin = 'true' ORDER BY idadminprof;");
+        while ($arr = pg_fetch_array ($resultatIdadmin)) {
+            $idadminFromBD = $arr['idadminprof'];
+            $valider = 'false';
+            foreach ($_POST['admin'] as $idadminprof)
+                if ($idadminprof == $idadminFromBD)
+                    $valider = 'true';
+            if ($valider != 'true' && $idadminFromBD != '0')
+                pg_query ($dbcon, requete_admin_devient_prof ($idadminFromBD));
+        }
+        header ('Location:./compte.php');
     }
-    header ('Location:./compte.php');
+    else {
+        foreach ($_POST['supprimer'] as $idetudiant)
+            pg_query ($dbcon, requete_supprimer_etudiant ($idetudiant));
+        header ('Location:./compte.php');
+    }
 }
 ?>
