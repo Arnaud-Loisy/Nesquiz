649cf54a9266c4c5c69a34961941ee83050cc541
mathieu <mathieuraoux@gmail.comt>
2013-05-30 01:12:18 +0200

    Page fini, etudiant

--- admin/xhr_compte.php
+++ admin/xhr_compte.php
@@ -3,28 +3,32 @@
 include '../bdd/connexionBDD.php';
 include '../bdd/requetes.php';
 
+session_start ();
 $dbcon = connexionBDD ();
 
 if ($_POST['utilisateur'] == "prof") {
-    unset ($_SESSION["etudiant"]);
+    if (isset ($_SESSION["etudiant"]))
+        unset ($_SESSION["etudiant"]);
     $_SESSION['enseignant'] = 1;
     echo"
-        <br>
-        <form action='majcompte.php' method='POST'>
-        <div class='scroll'>
-            <table class='liste'>
-                <thead>
-                    <th class='identifiant'> Identifiant </th>
-                    <th class='nom'> Nom </th> 
-                    <th class='prenom'> Prenom </th> 
-                    <th class='admin'> Admin </th> 
-                    <th class='supprimer'> Supprimer </th>
-                </thead>
-                <tbody>";
-    
+                <br>
+                <form action='majcompte.php' method='POST'>
+                    <table class='liste'>
+                        <thead>
+                            <th class='identifiant'> Identifiant </th>
+                            <th class='nom'> Nom </th> 
+                            <th class='prenom'> Prenom </th> 
+                            <th class='admin'> Admin </th> 
+                            <th class='supprimer'> Supprimer </th>
+                        </thead>
+                        </table>
+                <div class='scroll'>
+                    <table class='liste'>
+                        <tbody>";
+
     // Récupère l'identifiant, le nom et le prénom des admins et des profs
     $resultat = pg_query ($dbcon, requete_tous_idadminprof_nomadminprof_prenomadminprof ());
-    
+
     while ($arr = pg_fetch_array ($resultat)) {
         $nomadminprof = $arr["nomadminprof"];
         $prenomadminprof = $arr["prenomadminprof"];
@@ -47,32 +51,68 @@ if ($_POST['utilisateur'] == "prof") {
         echo"
                         <td class='supprimer'><input type='checkbox' name='supprimer[]' value='" . $idadminprof . "'></td>
                     </tr>";
-        
     }
     echo"
                 </tbody>
             </table>
         </div>
         <br>
-        <input  class='boutonMAJ' value='Valider' type='submit'>";
+        <input  class='boutonMAJ' value='Valider' type='submit'>
+        </form>
+        <form action='creercompte.php' method='POST'>
+            <br>
+            Créer compte enseignant
+            <table>
+                <tr>
+                    <td>Nom</td> 
+                    <td> <input name='nom' type='text' > </td> 
+                </tr>
+                <tr>
+                    <td>Prénom</td> 
+                    <td> <input name='prenom' type='text' > </td> 
+                </tr>
+                <tr>
+                    <td>Mot de passe</td> <td> 
+                    <input name='mdp' type='text' > </td> 
+                </tr>
+                <tr>
+                    <td>Langue de l'interface :</td> <td> <select name='langue'> 
+                            <option value='fr'>Français</option>
+                            <option value='en'>English</option>
+                        </select>  </td>
+                </tr>
+                <tr>
+                    <td colspan=2 style ='text-align: center'> <input type='checkbox' name='admin' value='1'> Admin </td>
+                </tr>
+            </table>
+            <input class='boutonCenter' value='Ajouter' type='submit'>
+
+        </form>
+    </div>
+</div>
+</body>
+</html>";
 }
 else {
-    unset ($_SESSION["enseignant"]);
+    if (isset ($_SESSION["enseignant"]))
+        unset ($_SESSION["enseignant"]);
     $_SESSION['etudiant'] = 1;
     echo"
         <br>
         <form action='majcompte.php' method='POST'>
+            <table class='liste'>
+                <thead>
+                    <th class='identifiant'>N° Etudiant</th>
+                    <th class='nom'>Nom</th>
+                    <th class='prenom'>Prenom</th>
+                    <th class='admin'>Promo</th>
+                    <th class='supprimer'>Supprimer</th>
+                </thead>
+             </table>
             <div class='scroll'>
                 <table class='liste'>
-                    <thead>
-                        <th class='identifiant'>N° Etudiant</th>
-                        <th class='nom'>Nom</th>
-                        <th class='prenom'>Prenom</th>
-                        <th class='admin'>Promo</th>
-                        <th class='supprimer'>Supprimer</th>
-                    </thead>
                 <tbody>";
-    $resultat = pg_query ($dbcon, requete_tous_les_etudiants());
+    $resultat = pg_query ($dbcon, requete_tous_les_etudiants ());
     while ($arr = pg_fetch_array ($resultat)) {
         $numero_etu = $arr['idetudiant'];
         $nom = $arr['nometudiant'];
@@ -80,11 +120,56 @@ else {
         $promo = $arr['promo'];
         echo"
             <tr>
-                <td class='identifiant'>" .$numero_etu. "</td>
-                <td class='nom'>" .$nom. "</td>
-                <td class='prenom'>" .$prenom. "</td>
-                <td class='admin'>".$promo. "</td>
-                <td class='supprimer'><input type='checkbox' name='supprimer[]' value='" .$numero_etu. "'></td>
+                <td class='identifiant'>" . $numero_etu . "</td>
+                <td class='nom'>" . $nom . "</td>
+                <td class='prenom'>" . $prenom . "</td>
+                <td class='admin'>" . $promo . "</td>
+                <td class='supprimer'><input type='checkbox' name='supprimer[]' value='" . $numero_etu . "'></td>
            </tr>";
     }
+    echo"
+                </tbody>
+            </table>
+        </div>
+        <br>
+        <input  class='boutonMAJ' value='Valider' type='submit'>
+        </form>
+        <form action='creercompte.php' method='POST'>
+            <br>
+            Créer compte étudiant
+            <table>
+                <tr>
+                    <td>Nom</td> 
+                    <td> <input name='nom' type='text' > </td> 
+                </tr>
+                <tr>
+                    <td>Prénom</td> 
+                    <td> <input name='prenom' type='text' > </td> 
+                </tr>
+                <tr>
+                    <td>N° Etudiant</td> <td> 
+                    <input name='num_etu' type='text' > </td> 
+                </tr>
+                <tr>
+                    <td>Promotion</td> <td> 
+                    <input name='promo' type='text' > </td> 
+                </tr>                
+                <tr>
+                    <td>Mot de passe</td> <td> 
+                        <input name='mdp' type='text' > </td> 
+                </tr>
+                <tr>
+                    <td>Langue de l'interface :</td> <td> <select name='langue'> 
+                            <option value='fr'>Français</option>
+                            <option value='en'>English</option>
+                        </select>  </td>
+                </tr>
+            </table>
+            <input class='boutonCenter' value='Ajouter' type='submit'>
+
+        </form>
+    </div>
+</div>
+</body>
+</html>";
 }
