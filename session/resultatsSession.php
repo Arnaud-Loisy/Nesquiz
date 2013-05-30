40d410c35443633ede727d0909241844b0d05b43
unknown <loisy.arnaud@gmail.com>
2013-05-30 10:30:36 +0200

    viewport

--- session/resultatsSession.php
+++ session/resultatsSession.php
@@ -3,6 +3,7 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8" />
+		<meta name="viewport" content="width=830">
 		<title>Résultats de la session</title>
                 <link rel="stylesheet" href="../styles/theme.css" />
         </head>
